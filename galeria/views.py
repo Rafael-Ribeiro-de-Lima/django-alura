4aba88cf44643019b650f48d60dc2abc665a94ca
Rafael <ribeirodelimarafael@gmail.com>
2024-02-18 15:08:20 -0300

    fim curso 3

--- galeria/views.py
+++ galeria/views.py
@@ -1,17 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from galeria.models import Fotografia
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
     fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
     return render(request, 'galeria/index.html', {'cards': fotografias})
 
 def imagem(request, foto_id):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
     fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
     if 'buscar' in request.GET: #"buscar" da requisição
         nome_a_buscar = request.GET['buscar'] #buscar é o name do formulário
